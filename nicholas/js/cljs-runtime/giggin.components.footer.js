1de198a09ccefc981497a59537ee4258dcad7c28
Lei Wu <wuleicanada@msn.com>
2020-05-06 18:24:24 -0400

    Clean up cljs-runtime

--- nicholas/js/cljs-runtime/giggin.components.footer.js
+++
@@ -1,7 +0,0 @@
-goog.provide('nicholas.components.footer');
-goog.require('cljs.core');
-nicholas.components.footer.footer = (function giggin$components$footer$footer(){
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"footer","footer",1606445390),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"img","img",1442687358),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"src","src",-1651076051),"img/giggin-icon.png",new cljs.core.Keyword(null,"alt","alt",-3214426),"Giggin logo"], null)], null)], null);
-});
-
-//# sourceMappingURL=nicholas.components.footer.js.map
